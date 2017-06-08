1fd72e5ff902d61110a04051c94248073505bbe4
Subhroneel Dey <t-sudey@microsoft.com>
2017-06-08 21:47:16 +0530

    done

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
 var gulpStylelint = require('gulp-stylelint');
 var fs = require('fs');
-var Writable = require('stream').Writable;
-var wstream = fs.createWriteStream('reports/eslint-errors/lintingerrors.csv');
+var eslint_wstream = fs.createWriteStream('reports/eslint-errors/lintingerrors.csv');
+//var sasslint_wstream = fs.createWriteStream('reports/sasslint-errors/sass-lintingerrors.csv');
 var css_path = "/**/*.css", html_path = "/**/*.html", js_path = "/**/*.js", sass_path = "/**/*.s+(a|c)ss";
 //Reads source paths from analyser_config.json
 gulp.task('readconfig', function () {
@@ -25,33 +25,42 @@ gulp.task('readconfig', function () {
   var jsonpath = JSON.parse(pathcontent);
   css_path = jsonpath.cssRoot + "/**/*.css";
   html_path = jsonpath.htmlRoot + "/**/*.html";
-  js_path = jsonpath.jsRoot +"/**/*.js" ;
-  sass_path = jsonpath.sassRoot +"/**/*.s+(a|c)ss";
+  js_path = jsonpath.jsRoot + "/**/*.js";
+  sass_path = jsonpath.sassRoot + "/**/*.s+(a|c)ss";
 
 });
 //For linting JavaScript files
-gulp.task('lint',['readconfig'] , () => {
+gulp.task('lint', ['readconfig'], () => {
   return gulp.src(js_path)
     .pipe(eslint())
-    .pipe(eslint.result(result => {
-        // Called for each ESLint result. 
-        console.log(`ESLint result: ${result.filePath}`);
-        console.log(`# Messages: ${result.messages.length}`);
-        console.log(`# Warnings: ${result.warningCount}`);
-        console.log(`# Errors: ${result.errorCount}`);
-    }))
-    .pipe(eslint.formatEach('stylish',wstream))
+    .pipe(eslint.result(result  =>  {
+              // Called for each ESLint result. 
+              console.log(`ESLint result: ${result.filePath}`);
+              console.log(`# Messages: ${result.messages.length}`);
+              console.log(`# Warnings: ${result.warningCount}`);
+              console.log(`# Errors: ${result.errorCount}`);
+        }))
+    .pipe(eslint.formatEach('stylish', eslint_wstream))
 });
 
 //For linting scss and sass
-gulp.task('sasslinting',['readconfig'] , function () {
-  return gulp.src(sass_path)
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
+gulp.task('sasslinting', ['readconfig'], function () {
+  var  file  =  fs.createWriteStream('reports/sass-linting-errors/lint_sass.csv');
+      var  stream  =  gulp.src(sass_path)
+            .pipe(sassLint({
+                  options:  {
+                        formatter:  'stylish'
+                  }
+            }))
+            .pipe(sassLint.format(file));
+      stream.on('finish',  function ()  {
+            file.end();
+      });
+  console.log("Sass Linting Errors output on reports/sass-linting-errors/lint_sass.csv");
+      return  stream;
 });
 //For linting css
-gulp.task('lint-css',['readconfig'] , function lintCssTask() {
+gulp.task('lint-css', ['readconfig'], function lintCssTask() {
   return gulp.src(css_path)
     .pipe(gulpStylelint({
       reporters: [
@@ -67,7 +76,7 @@ function checkcss(chunk) {
   test = test.replace(/[},{\.\s]/g, ' ');
   util.log(util.colors.yellow(test));
 }
-gulp.task('check-css-classname',['readconfig'] , function () {
+gulp.task('check-css-classname', ['readconfig'], function () {
   var checkdepen = transform(function (filename) {
     return map(function (chunk, next) {
       return next(null, checkcss(chunk.toString()))
@@ -81,7 +90,7 @@ function checkcss2(chunk) {
   test = test.replace(/[},{\.\s]/g, ' ');
   util.log(util.colors.yellow(test));
 }
-gulp.task('check-css-classname2',['readconfig'] , function () {
+gulp.task('check-css-classname2', ['readconfig'], function () {
   var checkdepen = transform(function (filename) {
     return map(function (chunk, next) {
       return next(null, checkcss2(chunk.toString()))
@@ -92,7 +101,7 @@ gulp.task('check-css-classname2',['readconfig'] , function () {
 
 })
 //For testing whether accessibility standards are satisfied
-gulp.task('test-accessibility',['readconfig'] , function () {
+gulp.task('test-accessibility', ['readconfig'], function () {
   return gulp.src([html_path, css_path])
     .pipe(access({
       force: true
@@ -105,7 +114,7 @@ gulp.task('test-accessibility',['readconfig'] , function () {
     .pipe(gulp.dest('reports/test-accessibility-errors/'));
 });
 //To prevent overwriting of libraries and check for dependencies
-gulp.task('checkdependency',['readconfig'] ,function () {
+gulp.task('checkdependency', ['readconfig'], function () {
   var checkdepen = transform(function (filename) {
     return map(function (chunk, next) {
       return next(null, checkangular(chunk.toString()))
@@ -115,5 +124,5 @@ gulp.task('checkdependency',['readconfig'] ,function () {
     .pipe(checkdepen)
 })
 gulp.task('default', ['readconfig'], function () {
-  runSequence(['lint', 'lint-css', 'sasslinting', 'checkdependency','test-accessibility'], 'check-css-classname', 'check-css-classname2')
+  runSequence(['lint', 'lint-css', 'sasslinting', 'checkdependency', 'test-accessibility'], 'check-css-classname', 'check-css-classname2')
 });
